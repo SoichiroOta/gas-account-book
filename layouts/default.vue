ae5c66e72d11bc6d43a026a7a01e5429b03eb800
Soichiro Ota <libarta.official@gmail.com>
2021-06-10 16:40:06 +0900

    chapter3

---
+++ layouts/default.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+    <!-- ツールバー -->
+    <v-app-bar app color="green" dark>
+      <!-- タイトル -->
+      <v-toolbar-title>{{ appName }}</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <!-- テーブルアイコンのボタン -->
+      <v-btn icon to="/">
+        <v-icon>mdi-file-table-outline</v-icon>
+      </v-btn>
+      <!-- 歯車アイコンのボタン -->
+      <v-btn icon to="/settings">
+        <v-icon>mdi-cog</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <!-- メインコンテンツ -->
+    <v-main>
+      <v-container fluid>
+        <nuxt/>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: mapState({
+    appName: state => state.settings.appName
+  }),
+
+  // Appインスタンス生成前に一度だけ実行されます
+  beforeCreate () {
+    this.$store.dispatch('loadSettings')
+  }
+}
+</script>
